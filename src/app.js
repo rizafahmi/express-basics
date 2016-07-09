204b04ea7b00edade6f462c7e4277dc2f1ed8724
Riza Fahmi <rizafahmi@gmail.com>
2016-07-09 20:36:52 +0700

    Add response to html

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ var express = require('express')
 var app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello, nurse!")
+  res.send("<h1>Hello, nurse!</h1>")
 })
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("The server running on http://localhost:3000/")
+})
