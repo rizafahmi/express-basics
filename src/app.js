5f42e59285d39bc0f09f92454c5782abc24301e7
Riza Fahmi <rizafahmi@gmail.com>
2016-07-09 20:49:58 +0700

    Add a new blog route with mocking data

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 'use strict'
 
-var express = require('express')
+const express = require('express')
+const posts = require('./mock/posts.json')
 
 var app = express()
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello, nurse!</h1>")
 })
+
+app.get("/blog", (req, res) => {
+  res.send(posts)
+})
+
 app.listen(3000, () => {
   console.log("The server running on http://localhost:3000/")
 })
